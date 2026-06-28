f0d73845dd68da09a50c4ff9bf4adde4a21749d9
AbidHasnine <abidhasnine12@gmail.com>
2026-06-28 12:49:16 +0600

    add : stripe secret key

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 require("dotenv").config();
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY environment variable is not set");
+}
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const app = express();
+
+app.use(express.json());
+app.use(cors());
